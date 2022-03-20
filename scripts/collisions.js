80c522127fab5bf37bd7301e7f1cebb46d72abde
Arthur Queiroz <arthurquimu@gmail.com>
2022-03-20 16:38:13 -0300

    Make initial mecanism for collision animation

--- scripts/collisions.js
+++ scripts/collisions.js
@@ -1,15 +1,21 @@
 // Important global variables
 const colCVWIDTH = 600;
 const colCVHEIGHT = 200;
-var PIDIG = 1000;
-var colDUR = 0; // ms
+var PIDIG = 0;
+var colDUR = 10; // ms
 var colrunning_function = 0;
 var interrupt_col = 0;
 var colis_paused = 0;
 var smallsize = 40;
 var bigsize = 80;
-var small_x = 70
-var big_x = 350
+var INIsmall_x = 250;
+var small_x = INIsmall_x;
+var INIbig_x = 350;
+var big_x = INIbig_x;
+var small_v = 0;
+var big_v = -1;
+var small_mass = 1;
+var big_mass = 1;
 
 // Create canvas and get its context.
 const colcanvas = document.getElementById('collision-simulator');
@@ -94,17 +100,45 @@ async function start_collision() {
 
     let favourable = 0;
     let pnum = parseInt(pi_digits.value);
-    if ( !isNaN(pnum) && pnum >= 1)
-        PIDIG = pnum
-    for (let i = 0; i < PIDIG; i++) {
-        if (colcreate_point()) {
-            favourable++;
+    if ( !isNaN(pnum) && pnum >= 0)
+        PIDIG = pnum;
+    big_mass = small_mass * Math.pow(100, PIDIG);    
+    while (true) {
+        if (interrupt_col) {
+            colstart_btn.textContent = 'Start';
+            colstart_btn.setAttribute('onclick', 'start_collision()');
+            colrunning_function = 0;
+            return;
         }
-        await sleep(DUR);
-        // By comparing areas, favourable/POINTNUM = pir^2/4r^2 = pi/4.
-        let ratio = favourable / (i+1);
-        colcur.textContent = `Number of collisions: ${favourable}/${i+1}`;
-        colratio_p.textContent = `Pi estimation: ${4 * ratio}`;
+        if (big_v > 0 && small_v <= big_v && ((small_v != 0 && small_x >= colCVWIDTH) || (small_v == 0 && big_x >= colCVWIDTH))) break;
+        await sleep(colDUR);
+        let xright = small_x + smallsize;
+        if (small_x <= 0) {
+            small_v = -small_v;
+            favourable++;
+            // alert(`${small_v} ${big_v}`);
+            colcur.textContent = `Number of collisions: ${favourable}`;
+            colratio_p.textContent = `Pi estimation: ${favourable / Math.pow(10, PIDIG)}`;
+        } else if (xright >= big_x) {
+            let newsmall_v = (small_mass * small_v - big_mass * small_v + 2 * big_mass * big_v) / (small_mass + big_mass);
+            let newbig_v = (2 * small_mass * small_v - small_mass * big_v + big_mass * big_v) / (small_mass + big_mass);
+            small_v = newsmall_v;
+            big_v = newbig_v;
+            favourable++;
+            colcur.textContent = `Number of collisions: ${favourable}`;
+            colratio_p.textContent = `Pi estimation: ${favourable / Math.pow(10, PIDIG)}`;
+            // alert(`${small_v} ${big_v}`);
+        }
+        // colctx.clearRect(small_x - 1, colCVHEIGHT - smallsize - 1, smallsize + 2, smallsize+2);
+        // colctx.clearRect(big_x-1, colCVHEIGHT - bigsize -1, bigsize+2, bigsize+2);
+        // colctx.stroke();
+        colreset_canvas();
+        small_x += small_v;
+        big_x += big_v;
+        colctx.strokeStyle = 'blue'
+        colctx.rect(small_x, colCVHEIGHT - smallsize, smallsize, smallsize);
+        colctx.rect(big_x, colCVHEIGHT - bigsize, bigsize, bigsize);
+        
         while (colis_paused) {
             await sleep(10);
             if (interrupt_col) {
@@ -122,12 +156,9 @@ async function start_collision() {
                 collision_pause(0);
             }
         }
-        if (interrupt_col) {
-            colstart_btn.textContent = 'Start';
-            colstart_btn.setAttribute('onclick', 'start_collision()');
-            colrunning_function = 0;
-            return;
-        }
+        colrunning_function = 0;
+        colstart_btn.textContent = 'Restart';
+        colstart_btn.setAttribute('onclick', 'start_collision()');
     }
     colrunning_function = 0;
     colstart_btn.textContent = 'Restart';
@@ -138,11 +169,13 @@ async function start_collision() {
 async function reset_collision() {
     let was_running = colrunning_function;
     interrupt_col = 1;
-    await sleep(10);
+    await sleep(100);
 
     colreset_canvas();
     colreset_indicators();
     interrupt_col = 0;
+    small_x = INIsmall_x;
+    big_x = INIbig_x;
     if (!was_running) {
         colstart_btn.textContent = 'Start';
     }
