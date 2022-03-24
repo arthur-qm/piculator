23230c56a77a4ccd45dcf5ef46ea30d9bd0ebf1c
Arthur Queiroz <arthurquimu@gmail.com>
2022-03-24 15:42:36 -0300

    Make modal with info about the page

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,3 +1,17 @@
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+var about_modal_container = document.getElementsByClassName('modal-container')[0];
+
+function about_modal() {
+    if (!about_modal_container.style.display || about_modal_container.style.display === 'none')
+        about_modal_container.style.display = 'block';
+    else
+        about_modal_container.style.display = 'none';
+}
+
+window.onclick = (ev) => {
+    if (ev.target == about_modal_container)
+        about_modal_container.style.display = 'none';
+};
